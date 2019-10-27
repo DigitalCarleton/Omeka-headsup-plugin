c53979ae119d20b85a4034ef5de6af4ca5041075
padillac <shinimaninima@gmail.com>
2019-10-27 13:33:59 -0500

    testing admin_dashboard hook

--- HeadsUpPlugin.php
+++ HeadsUpPlugin.php
@@ -1,1 +1,22 @@
 <?php
+
+
+class HeadsUpPlugin extends Omeka_Plugin_AbstractPlugin {
+
+  protected $_hooks = array(
+      'admin_dashboard'
+  );
+
+  protected $_filters = array(
+      'admin_navigation_main'
+  );
+
+
+  public function hookAdminDashboard(){
+    echo "<section>HELLLLOOOOOOO</section>"
+  }
+
+
+
+
+}

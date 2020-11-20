f6bcd21491b74f7fbf6282a3d97e5230298067be
Alvin Bierley <alvinbierley@eduroam-170-191.dyn.carleton.edu>
2020-11-20 17:05:49 -0600

    added functionality to display options based on user choice

--- HeadsUpPlugin.php
+++ HeadsUpPlugin.php
@@ -16,61 +16,73 @@ class HeadsUpPlugin extends Omeka_Plugin_AbstractPlugin {
 
   public function hookInstall()
   {
-      set_option('headsup_active', 1);
+      set_option('display_exhibits', 1);
+      set_option('display_exhibit_pages', 1);
+      set_option('display_items', 1);
+      set_option('display_collections', 1);
   }
 
   public function hookUninstall()
   {
-      delete_option('headsup_active');
+      delete_option('display_exhibits');
+      delete_option('display_exhibit_pages');
+      delete_option('display_items');
+      delete_option('display_collections');
   }
 
 
   public function filterAdminNavigationMain($navArray){
-
     $navArray['HeadsUp'] = array(
         'label' => __("HeadsUp"),
         'uri' => url('heads-up')
     );
-
     return $navArray;
   }
-
-
-
-  public function hookAdminDashboard(){
-    $headsup_active = get_option('headsup_active');
-    if ($headsup_active == 1) {
-
+  
+  /**
+   * counts the number of exhibits, exhibit pages, items, and collections and displays them
+   */
+  public function displayHeadsUp(){
       $exhibits = get_records('Exhibit', $params=array(), $limit=0);
       $numExhibits = count($exhibits);
+      $numItems = total_records('Item');
+      $numCollections = total_records('Collection');
 
-
-      $totalPages = 0;
+      $totalExhibitPages = 0;
       foreach ($exhibits as $key => $exhibit) {
         $pages = $exhibit->getPages();
-        $totalPages += count($pages);
+        $totalExhibitPages += count($pages);
       }
 
-
-
-
-
-      //print the data to the screen
-      echo "<section class='five columns omega'>
-      <div class='panel'>
-        <h2>Heads Up Info:</h2>
-        <p>Number of exhibits: {$numExhibits}</p>
-        <p>Total number of exhibit pages: {$totalPages}</p>
-      </div>
-      </section>";
-    }
+      // only displays information for the selected checkboxes
+      echo "<h2>Heads Up Info:</h2>";
+      if (get_option('display_exhibits') == 1) {
+        echo "<p>Number of exhibits: {$numExhibits}</p>";
+      }
+      if (get_option('display_exhibit_pages') == 1) {
+        echo "<p>Total number of exhibit pages: {$totalExhibitPages}</p>";
+      }
+      if (get_option('display_items') == 1) {
+        echo "<p>Number of items: {$numItems}</p>";
+      }
+      if (get_option('display_collections') == 1) {
+        echo "<p>Number of collections: {$numCollections}</p>";
+      }
   }
 
-
-
-
-
-
-
-
+  /**
+   * Hook to call relevant information to be displayed in an HTML section
+   */
+  public function hookAdminDashboard() {
+    ?>
+    <section class='five columns omega' style="margin-left:0%">
+      <div class='panel'>
+        <?php $this->displayHeadsUp() ?>
+      </div>
+    </section>
+    <?php
+  }
 }
+
+
+

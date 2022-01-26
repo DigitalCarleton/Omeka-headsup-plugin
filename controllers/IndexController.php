f66698c0e35b02ef201e569e3e5e6bc62895a4f7
Martin Bernard <nawm@carleton.edu>
2022-01-25 23:29:57 -0600

    Added code to display the date of most recent collection

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -11,7 +11,7 @@ class HeadsUp_IndexController extends Omeka_Controller_AbstractActionController
       if ($request->isPost()) {
           if ($form->isValid($request->getPost())) {
               $values = $form->getValues();
-              $options = array( 'display_exhibits', 'display_exhibit_pages', 'display_items', 'display_collections' );
+              $options = array( 'display_exhibits', 'display_exhibit_pages', 'display_items', 'display_collections', 'display_collection_date' );
               $i = 0;
               // iterates through each option to set value of 0 or 1
               foreach ($values as $value) {
--- libraries/HeadsUp/Form/Settings.php
+++ libraries/HeadsUp/Form/Settings.php
@@ -10,6 +10,7 @@ class HeadsUp_Form_Settings extends Omeka_Form
         $displayExhibitPages = get_option('display_exhibit_pages');
         $displayItems = get_option('display_items');
         $displayCollections = get_option('display_collections');
+        $displayCollectionDate = get_option('display_collection_date');
 
         // adds elements to form
         $this->addElement( 'checkbox', 'exhibits', 
@@ -23,6 +24,9 @@ class HeadsUp_Form_Settings extends Omeka_Form
 
         $this->addElement( 'checkbox', 'collections',
                             array( 'label' => 'Display number of Collections', 'value' => $displayCollections ) );
+        
+        $this->addElement( 'checkbox', 'collection_date',
+                            array( 'label' => 'Display date of most recent collection', 'value' => $displayCollectionDate ) );
 
         // builds the submit button
         $this->addElement('submit', 'submit', array(
@@ -38,6 +42,7 @@ class HeadsUp_Form_Settings extends Omeka_Form
         $this->addDisplayGroup( array('exhibit_pages'), 'Exhibit Pages' );
         $this->addDisplayGroup( array('items'), 'Items' );
         $this->addDisplayGroup( array('collections'), 'Collections' );
+        $this->addDisplayGroup( array('collection_date'), 'Date of recent collection' );
         $this->addDisplayGroup( array('submit'), 'Save Settings Button' );
     }
 }
